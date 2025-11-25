cc4706d97c1395c054924742781aeb38aaef4caf
LauraCelestino01 <laurabnc09@gmail.com>
2025-11-25 15:15:39 -0300

    Create simulador1.js

---
+++ simulador1.js
@@ -0,0 +1,503 @@
+const Lista = 
+[
+  {
+    "orgao": "Polícia Civil",
+    "data": "01/01/2024",
+    "valor": 5500.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Rodoviária Federal",
+    "data": "02/01/2024",
+    "valor": 6000.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Polícia Militar",
+    "data": "03/01/2024",
+    "valor": 4500.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Corpo de Bombeiros",
+    "data": "04/01/2024",
+    "valor": 7000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Guarda Municipal",
+    "data": "05/01/2024",
+    "valor": 3500.00,
+    "status": "falha",
+    "motivo": "Documentação incompleta"
+  },
+  {
+    "orgao": "Receita Federal",
+    "data": "06/01/2024",
+    "valor": 8000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Ministério Público",
+    "data": "07/01/2024",
+    "valor": 9000.00,
+    "status": "falha",
+    "motivo": "Erro no sistema"
+  },
+  {
+    "orgao": "Defesa Civil",
+    "data": "08/01/2024",
+    "valor": 4000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Detran",
+    "data": "09/01/2024",
+    "valor": 5000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Federal",
+    "data": "10/01/2024",
+    "valor": 6500.00,
+    "status": "falha",
+    "motivo": "Falta de recursos"
+  },
+  {
+    "orgao": "Ibama",
+    "data": "11/01/2024",
+    "valor": 5500.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Anvisa",
+    "data": "12/01/2024",
+    "valor": 6000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Inmetro",
+    "data": "13/01/2024",
+    "valor": 3000.00,
+    "status": "falha",
+    "motivo": "Dados incorretos"
+  },
+  {
+    "orgao": "Cetesb",
+    "data": "14/01/2024",
+    "valor": 7000.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Susep",
+    "data": "15/01/2024",
+    "valor": 8000.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Polícia Civil",
+    "data": "16/01/2024",
+    "valor": 5200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Rodoviária Federal",
+    "data": "17/01/2024",
+    "valor": 6100.00,
+    "status": "falha",
+    "motivo": "Erro humano"
+  },
+  {
+    "orgao": "Polícia Militar",
+    "data": "18/01/2024",
+    "valor": 4600.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Corpo de Bombeiros",
+    "data": "19/01/2024",
+    "valor": 7100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Guarda Municipal",
+    "data": "20/01/2024",
+    "valor": 3600.00,
+    "status": "falha",
+    "motivo": "Sistema fora do ar"
+  },
+  {
+    "orgao": "Receita Federal",
+    "data": "21/01/2024",
+    "valor": 8100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Ministério Público",
+    "data": "22/01/2024",
+    "valor": 9100.00,
+    "status": "falha",
+    "motivo": "Recursos insuficientes"
+  },
+  {
+    "orgao": "Defesa Civil",
+    "data": "23/01/2024",
+    "valor": 4100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Detran",
+    "data": "24/01/2024",
+    "valor": 5100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Federal",
+    "data": "25/01/2024",
+    "valor": 6600.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Ibama",
+    "data": "26/01/2024",
+    "valor": 5600.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Anvisa",
+    "data": "27/01/2024",
+    "valor": 6100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Inmetro",
+    "data": "28/01/2024",
+    "valor": 3100.00,
+    "status": "falha",
+    "motivo": "Dados incorretos"
+  },
+  {
+    "orgao": "Cetesb",
+    "data": "29/01/2024",
+    "valor": 7100.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Susep",
+    "data": "30/01/2024",
+    "valor": 8100.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Polícia Civil",
+    "data": "31/01/2024",
+    "valor": 5300.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Rodoviária Federal",
+    "data": "01/01/2024",
+    "valor": 6200.00,
+    "status": "falha",
+    "motivo": "Erro humano"
+  },
+  {
+    "orgao": "Polícia Militar",
+    "data": "02/01/2024",
+    "valor": 4700.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Corpo de Bombeiros",
+    "data": "03/01/2024",
+    "valor": 7200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Guarda Municipal",
+    "data": "04/01/2024",
+    "valor": 3700.00,
+    "status": "falha",
+    "motivo": "Sistema fora do ar"
+  },
+  {
+    "orgao": "Receita Federal",
+    "data": "05/01/2024",
+    "valor": 8200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Ministério Público",
+    "data": "06/01/2024",
+    "valor": 9200.00,
+    "status": "falha",
+    "motivo": "Recursos insuficientes"
+  },
+  {
+    "orgao": "Defesa Civil",
+    "data": "07/01/2024",
+    "valor": 4200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Detran",
+    "data": "08/01/2024",
+    "valor": 5200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Polícia Federal",
+    "data": "09/01/2024",
+    "valor": 6700.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Ibama",
+    "data": "10/01/2024",
+    "valor": 5700.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Anvisa",
+    "data": "11/01/2024",
+    "valor": 6200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Inmetro",
+    "data": "12/01/2024",
+    "valor": 3200.00,
+    "status": "falha",
+    "motivo": "Dados incorretos"
+  },
+  {
+    "orgao": "Cetesb",
+    "data": "13/01/2024",
+    "valor": 7200.00,
+    "status": "sucesso"
+  },
+  {
+    "orgao": "Susep",
+    "data": "14/01/2024",
+    "valor": 8200.00,
+    "status": "falha",
+    "motivo": "Problemas técnicos"
+  },
+  {
+    "orgao": "Polícia Civil",
+    "data": "15/01/2024",
+    "valor": 5400.00,
+    "status": "sucesso"
+  }
+];
+
+let Numeroexecucao = 1;
+
+for(let elemento of Lista){
+    console.log(`Repasse de número ${Numeroexecucao}`);
+    console.log(elemento);
+    Numeroexecucao++;
+}
+// #1
+
+let Numerofinal = Numeroexecucao - 1;
+console.log(`O total de repasses é ${Numerofinal}`);
+
+// #2
+// Total de repasses bem sussedidos
+const Sucessos=Lista.filter(item => item.status === `sucesso`);
+let SucessosTotal = 1
+for (const item of Sucessos) {
+    console.log(`Repasse com sucesso de número ${SucessosTotal}:`);
+    console.log(item); 
+    SucessosTotal++; 
+}
+let SucessoFinal = SucessosTotal -1;
+console.log(`O total de repasses com sucesso é ${SucessoFinal}`)
+
+// Por Orgão
+const OrgaoSucesso = Lista
+  .filter(item => item.status === "sucesso").reduce((acumulo, item) => {
+    acumulo[item.orgao] = (acumulo[item.orgao] || 0) + 1;
+    return acumulo;
+  }, {});
+
+const VROsucesso = Object.entries(OrgaoSucesso).map(([orgao, total]) => ({ orgao, total }));
+
+console.log(`Total de repasses de sucesso por orgãos:`)
+console.log(VROsucesso);
+
+// Valor total de repasses bem sussedidos
+let ValorTotal=0;
+for (const repasse of Sucessos) {
+  Valortotal=+ repasse.valor;
+
+};
+for (const item of Sucessos) {
+  console.log(item.valor)
+};
+console.log(`O valor total dos repasses de sucesso é ${Valortotal}`)
+
+// Por Orgão
+const ValoresOrgaoS = Lista
+.filter(item => item.status === "sucesso").reduce((acumulo, item) =>{
+  acumulo[item.orgao]= (acumulo[item.orgao] || 0)+ item.valor;
+  return acumulo
+}, {}) 
+const VOsucesso = Object.entries(ValoresOrgaoS).map(([orgao, total]) => ({orgao, total}));
+
+console.log(`O valor total que cada orgão recebeu foi`)
+console.log(ValoresOrgao)
+
+
+// Total de repasses com falhas
+const Falhas=Lista.filter(item => item.status === `falha`);
+let FalhasTotal = 1
+for (const item of Falhas) {
+    console.log(`Repasse com falha de número ${FalhasTotal}:`);
+    console.log(item); 
+    FalhasTotal++; 
+}
+let FalhaFinal = FalhasTotal -1;
+console.log(`O total de repasses com falhas é ${FalhaFinal}`)
+
+// Por Orgão
+const OrgaoFalha = Lista
+  .filter(item => item.status === "falha").reduce((acumulo, item) => {
+    acumulo[item.orgao] = (acumulo[item.orgao] || 0) + 1;
+    return acumulo;
+  }, {});
+
+const ROfalha = Object.entries(OrgaoFalha).map(([orgao, total]) => ({ orgao, total }));
+
+console.log(`Total de repasses de falhas por orgãos:`)
+console.log(ROfalha);
+
+//Por Motivo
+const Motivos=Lista.filter(item => item.status === "falha").map(item => item.motivo).reduce((acumulo, item) => {
+    acumulo[item.orgao] = (acumulo[item.orgao] || 0) + 1;
+    return acumulo;
+  }, {});
+const RMfalha = Object.entries(OrgaoFalha).map(([motivo, total]) => ({ motivo, total }));
+
+console.log(`Total de repasses de falhas por motivos`)
+console.log(RMfalha)
+
+// Valor total de repasses com falhas
+let ValortotalF=0;
+for (const repasse of Falhas) {
+  ValortotalF= ValortotalF + repasse.valor;
+
+};
+for (const item of Falhas) {
+  console.log(item.valor)
+};
+console.log(`O valor total dos repasses Com falhas é ${ValortotalF}`)
+
+// Por Orgão
+const OValoresFalhas = Lista
+.filter(item => item.status === "falha").reduce((acumulo, item) =>{
+  acumulo[item.orgao]= (acumulo[item.orgao] || 0)+ item.valor;
+  return acumulo
+}, {}) 
+const RVOfalhas = Object.entries(OValoresFalhas).map(([orgao, total]) => ({orgao, total}));
+
+console.log(`O valor total que cada orgão recebeu foi`)
+console.log(OValoresFalhas)
+
+//Por Motivo
+
+
+
+
+//#3
+// Detalhes do repasse com maior valor
+const MaiorValor = Lista.reduce((maior, item) => {
+  return item.valor > maior.valor ? item : maior;
+});
+
+//*console.log(MaiorValor)
+
+// Detalhes do repasse com menor valor
+const MenorValor = Lista.reduce((menor, item) => {
+  return item.valor < menor.valor ? item : menor;
+});
+
+console.log(MenorValor)
+
+// Dias com mais repasses
+
+const PorDia = Lista.reduce((acumulo, item) =>{
+  const Dia= item.data;
+  acumulo[Dia]= acumulo[Dia] ||+1;
+  return acumulo;
+}, {});
+
+console.log(`Quantidade de repasses no dia:`);
+console.log(PorDia);
+
+// Orgão com mais repasses
+const PorOrgaos = Lista.reduce((acumulo, item) =>{
+  const orgao= item.orgao;
+  acumulo[orgao]= acumulo[orgao] ||+1;
+  return acumulo;
+}, {});
+
+console.log(`Quantidade de repasses por orgãos:`);
+console.log(PorOrgaos);
+
+// Orgão com mais repasses de sucesso
+
+const PorOrgaoSucesso = Lista.filter(item => item.status === "sucesso").reduce((acumulo, item) => {
+    acumulo[item.orgao] = (acumulo[item.orgao] || 0) + 1;
+    return acumulo;
+  }, {});
+
+console.log(`Quantidade de repasses por orgãos de sucesso:`);
+console.log(PorOrgaoSucesso);
+
+// Orgãos com mais falhas
+
+const PorOrgaoFalha = Lista.filter(item => item.status === "falha").reduce((acumulo, item) => {
+    acumulo[item.orgao] = (acumulo[item.orgao] || 0) + 1;
+    return acumulo;
+  }, {});
+
+console.log(`Quantidade de repasses por orgãos com falhas:`);
+console.log(PorOrgaoFalha);
+
+// Motivos de falhas
+
+const FalhaMotivos = Lista.filter(item => item.status === "falha").reduce((acumulo, item) => {
+    acumulo[item.motivo] = (acumulo[item.motivo] || 0) + 1;
+    return acumulo;
+  }, {});
+
+
+console.log(`Quantidade de motivos de falha:`)
+console.log(FalhaMotivos);
+
+// #4
+
+let NomeOrgao= `Susep`; //ALTERAR NOME DESEJADO
+const DadosProcessados = Lista.filter(item => item.orgao === NomeOrgao);
+if (DadosProcessados.length === 0) {
+    console.log(`Nenhuma transação encontrada para este órgão.`);
+} else {
+    DadosProcessados.forEach((item, index) => {
+        console.log(`\n📌 Transação ${index + 1}:`);
+        console.log(`   Órgão:  ${item.orgao}`);
+        console.log(`   Data:   ${item.data}`);
+        console.log(`   Valor:  R$ ${item.valor.toFixed(2)}`);
+        console.log(`   Status: ${item.status}`);
+
+        if (item.status === "falha") {
+            console.log(`   Motivo da falha: ${item.motivo}`);
+        }
+    });
+}
